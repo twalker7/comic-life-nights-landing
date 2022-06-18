8ac6fdbd8b18ef44d6a91c997849c14115a1dbe7
Thomas Walker <thomasnwalker@yahoo.com>
2022-06-18 10:59:14 -0700

    asset update

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -10,23 +10,33 @@ function Content(){
     return(
         <div className="content-component">
             
-            <div>  
+  
               
-              <img id="jumbotron" src={require(`../../assets/flyer.jpg`)}/>
-            </div>
+              <img id="jumbotron" src={require(`../../assets/flyer.jpg`)} alt="Comic Life Nights 6/18/2022 Event Promotion"/>
+   
 
           <h1 className="section-title" id="meet-the-comics">  Meet the Comics </h1>
 
                     
            <div>
-               <img className="comic-feature" src ={require(`../../assets/comic-nick-alexander.jpg`)}/>
+               <img className="comic-feature" src ={require(`../../assets/comic-nick-alexander.jpg`)} alt="Comedian Nick Alexander"/>
             
             </div> 
  
 
                 
             <div>
-            <img className="comic-feature" src ={require(`../../assets/comic-ryan-cheaves.jpg`)}/>
+            <img className="comic-feature" src ={require(`../../assets/comic-ryan-cheaves.jpg`)} alt="Comedian Ryan Cheaves" />
+
+            </div> 
+
+            <div>
+            <img className="comic-feature" src ={require(`../../assets/comic-terrence-murphy.jpg`) } alt="Comedian Terrence Murphy Jr."/>
+
+            </div> 
+
+            <div>
+            <img className="comic-feature" src ={require(`../../assets/comic-del-harrison.jpeg`)} alt="Comedian Del Harrison"/>
 
             </div> 
 
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -10,13 +10,13 @@ function Navigation(props){
 
     return(
 <div className="navigation-container">
-  <div> <a href={false}> Home</a></div>
+  <div> <a href={''}> Home</a></div>
 
-  <div> <a href={false}> Upcoming  </a></div>
+  <div> <a href={''}> Upcoming  </a></div>
 
   <div> <a href={'#meet-the-comics'}> Meet the Comics </a> </div>
-  <div> <a href={false}> About </a> </div>
-  <div> <a href={false}> Contact/Submissions </a> </div>
+  <div> <a href={''}> About </a> </div>
+  <div> <a href={''}> Contact/Submissions </a> </div>
 
 
 
