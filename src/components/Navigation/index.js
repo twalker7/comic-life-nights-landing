73554fabd6c138fef866fbf2fa84e63c17b7201e
Thomas Walker <thomasnwalker@yahoo.com>
2022-06-26 09:53:38 -0700

    fixed bug of resetting state  -- working page switchesgit add -A

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -8,13 +8,13 @@ function Navigation(props){
 
     return(
 <div className="navigation-container">
-  <div> <a href={''} onClick={()=> props.setCurrentPage('home')}> Home</a></div>
+  <div> <a href={()=>false} onClick={()=> props.setCurrentPage('home')}> Home</a></div>
 
-  <div> <a href={''} onClick={()=> props.setCurrentPage('upcoming')}> Upcoming  </a></div>
+  <div> <a href={()=>false} onClick={()=> props.setCurrentPage('upcoming')}> Upcoming  </a></div>
 
   <div> <a href={'#meet-the-comics'}> Meet the Comics </a> </div>
-  <div> <a href={''} onClick={()=> props.setCurrentPage('about')}> About </a> </div>
-  <div> <a href={''} onClick={()=> props.setCurrentPage('contact')}> Contact/Submissions </a> </div>
+  <div> <a href={()=>false} onClick={()=> props.setCurrentPage('about')}> About </a> </div>
+  <div> <a href={()=>false} onClick={()=> props.setCurrentPage('contact')} > Contact/Submissions </a> </div>
   
 
 </div>
