9ad752aa007fce32b90aa2dc7abc8c2c3f775859
Thomas Walker <thomasnwalker@yahoo.com>
2022-06-26 03:11:09 -0700

    decent styling for contact links

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,10 +4,13 @@ import React from 'react'
 function Contact(){
     return(
         <div>
-            <a className="email-link" href="mailto:thomasnwalker@yahoo.com"> Submit Content  </a>
-          
+           <div className="link-container">
+           <a className="email-link" href="mailto:thomasnwalker@yahoo.com"> Submit Content  </a>
            <a className="instagram-link" href=""> <img className ="instagram-logo" src={require("../../assets/instagram-logo.jpg")} /> Instagram  </a>
-          <h2> Submission Section Coming Soon </h2> 
+
+            </div> <br/> 
+
+          
         </div>
     );
 }
