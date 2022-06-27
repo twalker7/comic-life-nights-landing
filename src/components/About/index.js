892cfe4f95aa0fb7190f12d678da8540b129b460
Thomas Walker <thomasnwalker@yahoo.com>
2022-06-27 14:51:09 -0700

    fixed border between nav and page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -4,8 +4,9 @@ import React from 'react';
 function About(){
     return(
         <div className="page-component">
-           <h1 className="section-title"> About </h1> 
+           <h1 className="section-title"> About Comic Life Nights  </h1> 
            <img src={require('../../assets/comic-life-nights-live-shot.jpeg')}/>
+        
         </div>
     );
 }
