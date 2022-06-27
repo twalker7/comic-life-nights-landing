bbb9bed0857bd3afa0285515c459f0de3c1acd61
Thomas Walker <thomasnwalker@yahoo.com>
2022-06-27 01:09:30 -0700

    added link

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,8 +7,9 @@ function Contact(){
 
             <h1 className="section-title"> Contact Us </h1>
            <div className="link-container">
-           <a className="email-link" href="mailto:delise1881@gmail.com"> Submit Content  </a>
-           <a className="instagram-link" href=""> <img className ="instagram-logo" src={require("../../assets/instagram-logo.jpg")} /> </a>
+           <a className="email-link" href="mailto:delise1881@gmail.com" target="https://instagram.com/comiclifenights?igshid=YmMyMTA2M2Y="> Submit Content  </a>
+           <a className="instagram-link" href="https://instagram.com/comiclifenights?igshid=YmMyMTA2M2Y=
+"> <img className ="instagram-logo" src={require("../../assets/instagram-logo.jpg")} /> </a>
 
             </div> 
 
--- src/components/Upcoming/index.js
+++ src/components/Upcoming/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Upcoming(){
     return(
         <div className="page-component">
-An Array of Upcoming shows 
+            <h1 className="section-title"> Upcoming </h1>
         </div>
     );
 }
