e076f3b1b6ec27d3404164bca79a95f0bca180df
Thomas Walker <thomasnwalker@yahoo.com>
2022-06-28 00:20:56 -0700

    fixed instagram logo

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,17 +1,25 @@
 import React from 'react'
-
+import {Row, Col, Button} from 'react-bootstrap';
 
 function Contact(){
     return(
         <div className="page-component">
 
             <h1 className="section-title"> Contact Us </h1>
+         <Row> 
+             <Col/>
+           <Col>
            <div className="link-container">
-           <a className="email-link" href="mailto:delise1881@gmail.com" target="https://instagram.com/comiclifenights?igshid=YmMyMTA2M2Y="> Submit Content  </a>
-           <a className="instagram-link" href="https://instagram.com/comiclifenights?igshid=YmMyMTA2M2Y="> <img className ="instagram-logo" src={require("../../assets/instagram-logo.jpg")} /> </a>
-
+         
+                 <a className="email-link" href="mailto:delise1881@gmail.com" target="https://instagram.com/comiclifenights?igshid=YmMyMTA2M2Y="> Submit Content  </a>
+         
+         
+           <a className="instagram-link" href="https://instagram.com/comiclifenights?igshid=YmMyMTA2M2Y="> <img className ="instagram-logo" src={require("../../assets/instagram-logo2.png")} /> </a>
+        
             </div> 
-
+            </Col > 
+            <Col/>
+            </Row>
           
         </div>
     );
