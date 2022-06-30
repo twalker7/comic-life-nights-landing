0db2584352dd1384198f285104627cfd7b5771cb
Thomas Walker <thomasnwalker@yahoo.com>
2022-06-30 05:11:13 -0700

    changed about asset

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,7 +6,8 @@ function About(){
         <div className="page-component">
            <h1 className="section-title"> About Comic Life Nights  </h1> 
            <p className="about-summary"> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-           <img className="about-photo" src={require('../../assets/comic-life-nights-live-shot.jpeg')}/>
+           <h3 className="section-subtitle"> Creator </h3>
+           <img className="about-photo" src={require('../../assets/comic-life-nights-about.jpeg')}/>
         </div>
     );
 }
