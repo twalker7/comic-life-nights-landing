5e896b86bc0ef3e2075bce25a7b842b788703ce0
Thomas Walker <thomasnwalker@yahoo.com>
2022-06-30 18:10:34 -0700

    about section details

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,6 +7,7 @@ function About(){
            <h1 className="section-title"> About Comic Life Nights  </h1> 
            <p className="about-summary"> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
            <h3 className="section-subtitle"> Creator </h3>
+           <h6 className="sub-subtitle"> Delise Aubrey </h6>
            <img className="about-photo" src={require('../../assets/comic-life-nights-about.jpeg')}/>
         </div>
     );
