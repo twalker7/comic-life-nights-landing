abc3addea258f9f1a5f05846a20a268a2483d18a
Thomas Walker <thomasnwalker@yahoo.com>
2022-07-18 14:56:10 -0700

    buffer animation

--- src/components/Upcoming/index.js
+++ src/components/Upcoming/index.js
@@ -4,6 +4,11 @@ function Upcoming(){
     return(
         <div className="page-component">
             <h1 className="section-title"> Upcoming </h1>
+            <div className="circuit-container"> 
+                <div className="circuit"> 
+                    <div className="circuit-propellor" > </div>
+                </div>
+            </div>
         </div>
     );
 }
