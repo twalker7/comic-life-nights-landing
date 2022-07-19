41f6f0846953b2b6fa86074eb22d62996b70cc2f
Thomas Walker <thomasnwalker@yahoo.com>
2022-07-18 18:47:42 -0700

    upcoming events subtitle

--- src/components/Upcoming/index.js
+++ src/components/Upcoming/index.js
@@ -4,6 +4,7 @@ function Upcoming(){
     return(
         <div className="page-component upcoming">
             <h1 className="section-title"> Upcoming </h1>
+            <h4 className="section-subtitle"> Coming Soon </h4>
             <div className="circuit-container"> 
                 <div className="circuit"> 
                     <div className="circuit-propellor" > </div>
